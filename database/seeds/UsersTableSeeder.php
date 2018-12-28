8c601f3cc8c8a99ed9bc9a8d678e4f61fa6cecd4
artqz <djoctuk@yandex.ru>
2018-12-28 13:50:55 +0500

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,5 +15,7 @@ class UsersTableSeeder extends Seeder
     {
         User::create(['name' => 'Убивака', 'email' => 'djoctuk@yandex.ru', 'password' => Hash::make(110789)]);
         User::create(['name' => 'Chlens', 'email' => 'test@test.ru', 'password' => Hash::make(110789)]);
+        User::create(['name' => 'Jesus', 'email' => 'fox.toddtester@gmail.com', 'password' => Hash::make(12345)]);
+        User::create(['name' => 'Anrew', 'email' => 'hotmottor@gmail.com', 'password' => Hash::make(12345)]);
     }
 }
