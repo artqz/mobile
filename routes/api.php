791573c07f6d7a8c059e63b72693da7cf3e0fa34
artqz <djoctuk@yandex.ru>
2019-01-04 17:01:55 +0500

    update api

--- routes/api.php
+++ routes/api.php
@@ -29,8 +29,8 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('shops/location/{id}', 'API\ShopController@location');
     Route::apiResources(['shops' => 'API\ShopController']);
     Route::apiResources(['items' => 'API\ItemController']);
+    Route::post('users/equip_item', 'API\UserController@equip_item');
+    Route::apiResources(['users' => 'API\UserController']);
 });
-Route::apiResources(['users' => 'API\UserController']);
-  Route::post('users/equip_item', 'API\UserController@equip_item');
 Route::apiResources(['locations' => 'API\LocationController']);
 
